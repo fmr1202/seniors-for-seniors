6e06221a233db753c5915a08878709d58d7aeb52
fmr1202 <frescign@oswego.edu>
2020-11-04 13:10:15 -0500

    updates

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,11 @@
 function showNav() {
     if (document.getElementById('theDrawer').style.display === "block"){
+        document.getElementById('nav-button').src = "assets/nav-open.svg";
         document.getElementById('theDrawer').style.display = "none";
     }
     else{
         document.getElementById('theDrawer').style.display = "block";
+        document.getElementById('nav-button').src = "assets/nav-close.svg";
     }
         
     }
